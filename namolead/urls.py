ca073a3eedbdc07de7baeec8379382be6ea10a89
PranavAgarkar07 <pranavagarkar8@gmail.com>
2026-08-19 10:13:50 +0530

    fix(deploy): serve /media/ in production (renditions 404'd without nginx)

--- namolead/urls.py
+++ namolead/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.static import serve as media_serve
 from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 
@@ -17,5 +18,9 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        path("media/<path:path>", media_serve, {"document_root": settings.MEDIA_ROOT})
+    ]
 
 urlpatterns += [path("", include(wagtail_urls))]
